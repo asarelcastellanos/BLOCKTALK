619894df80d43307b443cecf426f9697a3b2f022
Asarel Castellanos <asarelc@gmail.com>
2022-07-23 20:00:39 -0700

    added bottomtab
    now need to start working on chat and individualchat

--- src/navigation/UserStack.js
+++ src/navigation/UserStack.js
@@ -1,20 +1,74 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+// Screens
+import MapScreen from "../screens/MapScreen";
 import ChatScreen from "../screens/ChatScreen";
-import HomeScreen from "../screens/HomeScreen";
+import CameraScreen from "../screens/CameraScreen";
+import StoriesScreen from "../screens/StoriesScreen";
+import SpotlightScreen from "../screens/SpotlightScreen";
 
-
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function UserStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Chat" component={ChatScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName="Camera"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, size }) => {
+            let iconName;
+            let iconColor;
+
+            if (route.name == "Map") {
+              iconName = "ios-location-outline";
+              iconColor = focused ? "green" : "grey";
+            } else if (route.name === "Chat") {
+              iconName = "ios-chatbox-outline";
+              iconColor = focused ? "#2b83b3" : "grey";
+            } else if (route.name === "Camera") {
+              iconName = focused ? "ios-scan-outline" : "ios-camera-outline";
+              iconColor = focused ? "yellow" : "grey";
+            } else if (route.name === "Stories") {
+              iconName = "ios-people-outline";
+              iconColor = focused ? "purple" : "grey";
+            } else if (route.name === "Spotlight") {
+              iconName = "ios-play-outline";
+              iconColor = focused ? "red" : "grey";
+            }
+            return <Ionicons name={iconName} size={size} color={iconColor} />;
+          },
+        })}
+      >
+        <Tab.Screen
+          name="Map"
+          component={MapScreen}
+          options={{ tabBarShowLabel: false }}
+        />
+        <Tab.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={{ tabBarShowLabel: false }}
+        />
+        <Tab.Screen
+          name="Camera"
+          component={CameraScreen}
+          options={{ tabBarShowLabel: false }}
+        />
+        <Tab.Screen
+          name="Stories"
+          component={StoriesScreen}
+          options={{ tabBarShowLabel: false }}
+        />
+        <Tab.Screen
+          name="Spotlight"
+          component={SpotlightScreen}
+          options={{ tabBarShowLabel: false }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,54 +1,66 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { GiftedChat } from "react-native-gifted-chat";
-import db from "../../firebase";
-import { doc, onSnapshot, updateDoc, arrayUnion } from "firebase/firestore";
-import { useAuthentication } from "../utils/hooks/useAuthentication";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { getAuth, signOut } from "firebase/auth";
 
+export default function ChatScreen({ navigation }) {
+  const [state, setState] = useState();
 
-export default function ChatScreen({ navigation, route }) {
-  const [messages, setMessages] = useState([]);
-  const chatRef = doc(db, "Chats", "myfirstchat");
+  const auth = getAuth();
+  const user = auth.currentUser;
 
-  const { user, userData } = useAuthentication();
+  console.log(user, "<--- user in the home screen");
 
-  useEffect(() => {
-    let unsubscribeFromNewSnapshots = onSnapshot(
-      chatRef,
-      (doc) => {
-        console.log("New Snapshot!");
-        setMessages(doc.data().messages);
-      }
+  if (user !== null) {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.logoutBtn}
+          onPress={() => {
+            signOut(auth)
+              .then(() => {
+                // Sign-out successful.
+                user = null;
+              })
+              .catch((error) => {
+                // An error happened.
+                // should we do something with that error??
+              });
+          }}
+        >
+          <Text style={styles.loginText}>sign out</Text>
+        </TouchableOpacity>
+
+        <Text>Hello, {user.email}! </Text>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+          <Text style={styles.item}>Chat</Text>
+        </TouchableOpacity>
+      </View>
     );
-
-    return function cleanupBeforeUnmounting() {
-      unsubscribeFromNewSnapshots();
-    };
-  }, []);
-
-  const onSend = useCallback((messages = []) => {
-    updateDoc(chatRef, {
-      // arrayUnion appends the message to the existing array
-      messages: arrayUnion(messages[0]),
-    });
-    // setMessages((previousMessages) =>
-    //   GiftedChat.append(previousMessages, messages)
-    // );
-  }, []);
-  if(!user || !userData) {
-    return <View></View>
   }
-  return (
-    <GiftedChat
-      messages={messages}
-      onSend={(messages) => onSend(messages)}
-      user={{
-        _id: user.uid,
-        name: userData.name
-      }}
-      inverted={false}
-      showUserAvatar={true}
-      renderUsernameOnMessage={true}
-    />
-  );
 }
+
+const styles = StyleSheet.create({
+  logoutBtn: {
+    width: "50%",
+    borderRadius: 25,
+    margin: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "grey",
+    color: "white",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    backgroundColor: "yellow",
+    borderRadius: 25,
+    margin: 20,
+  },
+});
