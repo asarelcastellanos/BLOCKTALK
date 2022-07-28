a0e06340326145d6920d4aed765849350dca8128
vmalabanan <vanessamalabanan@yahoo.com>
2022-07-28 13:48:24 -0700

    add carousel component

--- src/screens/StoriesScreen.js
+++ src/screens/StoriesScreen.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
+import Carousel from "react-native-snap-carousel";
 
 export default function StoriesScreen() {
   return (
     <View>
-        <Text>
-            StoriesScreen
-        </Text>
+      <Text>StoriesScreen</Text>
     </View>
-  )
+  );
 }
