dedd1371a6591557460d9943571ce15fda1e923e
rainawan <rainawan02@gmail.com>
2022-07-28 00:04:43 -0700

    add button, can't take pic

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,11 +1,39 @@
 import React, { useState, useEffect} from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { Camera, CameraType } from 'expo-camera';
+import { useIsFocused } from '@react-navigation/native';
+
+const recordVideo = async () => {
+  if(cameraRef) {
+      try{
+          const options = {maxDuration: 60, quality: Camera.Constants.VideoQuality['480']}
+          const videoRecordPromise = cameraRef.recordAsync(options)
+          if(videoRecordPromise) {
+              const data = await videoRecordPromise;
+              const source = data.uri
+          }
+      } catch(error) {
+          console.warn(error)
+      }
+  }
+}
+
+const stopVideo = async () => {
+  if(cameraRef) {
+      cameraRef.stopRecording()
+  }
+}
 
 export default function CameraScreen() {
 
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(CameraType.back);
+  const [CameraRef, setCameraRef] = useState(null)
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [cameraFlash, setCameraFlash] = useState(Camera.Constants.FlashMode.off);
+  const [isCameraReady, setIsCameraReady] = useState(false)
+
+  const isFocused = useIsFocused()
 
   useEffect(() => {
     (async () => {
@@ -13,6 +41,7 @@ export default function CameraScreen() {
       setHasPermission(status === 'granted');
     })();
   }, []);
+
 
   if (hasPermission === null) {
     return <View />;
@@ -33,6 +62,29 @@ export default function CameraScreen() {
           </TouchableOpacity>
         </View>
       </Camera>
+
+      {isFocused ?
+          <Camera 
+            style={styles.camera} 
+            type={type}
+            ref={ref => setCameraRef(ref)}
+            style={styles.camera}
+            ratio={'16:9'}
+            type={cameraType}
+            flashMode={cameraFlash}
+            onCameraReady = {() => setIsCameraReady(true)}
+            />
+            : null}
+
+            <View styles={styles.recordButtonContainer}>
+                <TouchableOpacity 
+                    disabled={!isCameraReady}
+                    onLongPress={() => recordVideo()}
+                    onPressOut={() => stopVideo}
+                    style={styles.recordButton}
+                />
+            </View>
+
     </View>
   );
 }
@@ -59,4 +111,16 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: 'white',
   },
+  recordButtonContainer: {
+    flex: 1,
+    marginHorizontal: 30
+  }, 
+  recordButton: {
+    borderWidth: 8,
+    borderColor: '#fff',
+    borderRadius: 100,
+    height: 80,
+    width: 80,
+    alignSelf: 'center'
+  }
 });
