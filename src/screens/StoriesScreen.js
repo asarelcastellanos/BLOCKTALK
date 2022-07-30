fbce06ffad4da951a4e034d5540624692c16c499
vmalabanan <vanessamalabanan@yahoo.com>
2022-07-30 15:38:09 -0700

    add layout to stories page

--- src/screens/StoriesScreen.js
+++ src/screens/StoriesScreen.js
@@ -1,5 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+// import { View, Text } from "react-native";
+import { View } from "react-native";
+import {
+  NativeBaseProvider,
+  Container,
+  Heading,
+  Box,
+  Center,
+  VStack,
+  HStack,
+  Text,
+  Circle,
+  ScrollView,
+} from "native-base";
 
 // Carousel
 import { SafeAreaView, StyleSheet } from "react-native";
@@ -7,18 +20,90 @@ import CarouselCards from "../../CarouselCards";
 
 export default function StoriesScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <CarouselCards />
-    </SafeAreaView>
+    // <SafeAreaView style={styles.container}>
+    //   <CarouselCards />
+    // </SafeAreaView>
+
+    <NativeBaseProvider>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Container style={styles.friends}>
+          <Text bold fontSize="md">
+            Friends
+          </Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <HStack space={3} justifyContent="center">
+              <Circle size="74px" bg="light.300" />
+              <Circle size="74px" bg="light.300" />
+              <Circle size="74px" bg="light.300" />
+              <Circle size="74px" bg="light.300" />
+              <Circle size="74px" bg="light.300" />
+              <Circle size="74px" bg="light.300" />
+            </HStack>
+          </ScrollView>
+        </Container>
+
+        <Container style={styles.hugs}>
+          <Text bold fontSize="md">
+            HUGS
+          </Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <HStack space={9}>
+              <VStack space={3} justifyContent="center">
+                <Center h="60" w="330" bg="light.300" rounded="2xl" />
+                <Center h="60" w="330" bg="light.300" rounded="2xl" />
+                <Center h="60" w="330" bg="light.300" rounded="2xl" />
+              </VStack>
+
+              <VStack space={3} justifyContent="center">
+                <Center h="60" w="330" bg="light.300" rounded="2xl" />
+                <Center h="60" w="330" bg="light.300" rounded="2xl" />
+                <Center h="60" w="330" bg="light.300" rounded="2xl" />
+              </VStack>
+            </HStack>
+          </ScrollView>
+        </Container>
+
+        <Container style={styles.discover}>
+          <Text bold fontSize="md">
+            Discover
+          </Text>
+          <VStack space={3}>
+            <HStack space={3}>
+              <Center h="263" w="177" bg="light.300" rounded="2xl" />
+              <Center h="263" w="177" bg="light.300" rounded="2xl" />
+            </HStack>
+
+            <HStack space={3}>
+              <Center h="263" w="177" bg="light.300" rounded="2xl" />
+              <Center h="263" w="177" bg="light.300" rounded="2xl" />
+            </HStack>
+          </VStack>
+        </Container>
+      </ScrollView>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: 200,
-    backgroundColor: "#fff",
-    alignItems: "flex-end",
-    justifyContent: "flex-end",
-    // padding: 50,
+  // container: {
+  //   height: 200,
+  //   backgroundColor: "#fff",
+  //   alignItems: "flex-end",
+  //   justifyContent: "flex-end",
+  //   // padding: 50,
+  // },
+
+  friends: {
+    margin: 16,
+    minWidth: 400,
+  },
+
+  hugs: {
+    margin: 16,
+    minWidth: 400,
+  },
+
+  discover: {
+    margin: 16,
   },
 });
