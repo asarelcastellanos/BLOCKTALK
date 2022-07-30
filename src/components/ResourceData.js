94f29e9fe482f7c2a67ade99eae14f3b6aa8327a
rainawan <rainawan02@gmail.com>
2022-07-30 15:24:28 -0700

    use props for resourcesScreen

---
+++ src/components/ResourceData.js
@@ -0,0 +1,25 @@
+var resourceData = [
+    {
+        "key": "1",
+        "name": "nonprofit1",
+        "description": "description1"
+
+    },
+    {
+        "key": "2",
+        "name": "nonprofit2",
+        "description": "description2"
+    },
+    {
+        "key": "3",
+        "name": "nonprofit2",
+        "description": "description2"
+    },
+    {
+        "key": "4",
+        "name": "nonprofit2",
+        "description": "description2"
+    }
+]
+
+module.exports = resourceData;
--- src/screens/ResourceScreen.js
+++ src/screens/ResourceScreen.js
@@ -1,40 +1,73 @@
 import { SafeAreaView, View, Text, StyleSheet, FlatList, Dimensions, Image } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
-import React, {useState, useEffect } from 'react';
-import { ActivityIndicator } from 'react-native-web';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, {useState, useEffect, Component } from 'react';
+// import { ActivityIndicator } from 'react-native-web';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
+import resourceData from '../components/ResourceData';
+import { render } from 'react-dom';
 
-export default function StoriesScreen({ navigation, focused }) {
-  const isFocused = useIsFocused();
-
-  const [resource, setResource] = useState([
-    {name: 'a', id: '1'},
-    {name: 'b', id: '2'},
-    {name: 'c', id: '3'},
-    {name: 'd', id: '4'},
-    {name: 'e', id: '5'},
-    {name: 'f', id: '6'},
-    {name: 'g', id: '7'},
-  ]);
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        keyExtractor={(item) => item.id}
-        data={resource}
-        renderItem={({item}) => (
-          <Text style={styles.item}>{item.name}</Text>
-        )}
-      />
-    </View>
-
-    // <View style={styles.container}>
-    //   {isFocused?
-    //     <Text>Resource page</Text>
-    //   : null}
-    // </View>
-  )
+class FlatListItem extends Component {
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        backgroundColor: 'yellow'
+      }}>
+        <Text style={styles.flatListItem}>{this.props.item.name}</Text>
+        <Text style={styles.flatListItem}>{this.props.item.description}</Text>
+      </View>
+    );
+  }
 }
+
+export default class ResourceScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={resourceData}
+          renderItem={({item, index}) => {
+            return (
+              <FlatListItem item={item} index={index}>
+              </FlatListItem>
+            );
+          }}
+        >
+        </FlatList>
+      </View>
+    );
+  }
+}
+
+// export default function ResourceScreen({ navigation, focused }) {
+//   const isFocused = useIsFocused();
+
+//   const [resource, setResource] = useState([
+//     {name: 'a', id: '1'},
+//     {name: 'b', id: '2'},
+//     {name: 'c', id: '3'},
+//     {name: 'd', id: '4'},
+//     {name: 'e', id: '5'},
+//     {name: 'f', id: '6'},
+//     {name: 'g', id: '7'},
+//   ]);
+
+//   return (
+//     <View style={styles.container}>
+//       {isFocused?
+//         <Text>Resource page</Text>
+//       : null}
+
+//       <FlatList
+//         keyExtractor={(item) => item.id}
+//         data={resource}
+//         renderItem={({item}) => (
+//           <Text style={styles.item}>{item.name}</Text>
+//         )}
+//       />
+//     </View>
+//   )
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -42,12 +75,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     height: Dimensions.get('window').height,
   },
-  item: {
-    marginTop: 24,
+  flatListItem: {
+    // marginTop: 24,
     marginVertical: 8,
     marginHorizontal: 16,
     padding: 30,
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
     fontSize: 24
   }
 })
