6d968e70d30bc9375211a7ec8f5e81330e7d0fa4
Trung N Vong <trungnvong956@g.ucla.edu>
2022-08-03 12:23:25 -0700

    Polished Recipes Screen

--- src/screens/SpotlightScreen.js
+++ src/screens/SpotlightScreen.js
@@ -1,88 +1,110 @@
 import React from "react";
-import { View, Image, StyleSheet, Text } from "react-native";
+// import { View, StyleSheet, Text } from "react-native";
 import { TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Box,
+  Heading,
+  Center,
+  HStack,
+  VStack,
+  ScrollView,
+  Image,
+} from "native-base";
+import Chef from "../../assets/img/chef.png";
+import MainImg from "../../assets/img/img_4.png";
+import FactsImg from "../../assets/img/img_02.png";
+import RecipesImg from "../../assets/img/img_05.png";
 
 export default function SpotlightScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <View style={styles.textTitle}>
-        <Text style={styles.textTitle2}>Welcome</Text>
-        <Text style={styles.textTitle2}>Snap Chopper</Text>
-      </View>
+    <View backgroundColor={"gray.800"}>
+      <Center>
+        <Text
+          fontSize={35}
+          fontWeight={"bold"}
+          fontFamily={"Graphik-Medium"}
+          color={"white"}
+        >
+          Welcome
+        </Text>
+        <Text
+          fontSize={35}
+          fontWeight={"bold"}
+          fontFamil={"Graphik-Medium"}
+          color={"white"}
+        >
+          Snap Chopper
+        </Text>
+        <Image source={MainImg} alt={"img"} size="2xl" />
+      </Center>
+      <Text
+        fontSize={25}
+        fontWeight={"bold"}
+        fontFamily={"Graphik-Medium"}
+        ml={6}
+        color={"white"}
+      >
+        Let's get ...
+      </Text>
+      <View ml={16} mt={3}>
+        <HStack mb={2}>
+          <TouchableOpacity onPress={() => navigation.navigate("Cooking")}>
+            <Box bg={"white"} borderRadius={30} mr="5">
+              <Image source={RecipesImg} alt={"img"} size="xs" />
+            </Box>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Cooking")}>
+            <Box>
+              <Text
+                fontSize={20}
+                fontWeight={"bold"}
+                fontFamily={"Graphik-Medium"}
+                color={"white"}
+              >
+                Cookin's with Chefs
+              </Text>
+            </Box>
+          </TouchableOpacity>
+        </HStack>
 
-      <View style={styles.imgSection}>
-        <Image
-          style={styles.Img}
-          source={require("../../assets/img/img_4.png")}
-        />
-      </View>
-      <View style={styles.midText}>
-        <Text style={styles.textInputHead}>Let's get ...</Text>
-      </View>
-      <View style={styles.btnContainer}>
-        <TouchableOpacity
-          style={styles.btn}
-          onPress={() => navigation.navigate("Cooking")}
-        >
-          <Text style={styles.textInput}>Cookin's with Chefs</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.btn}
-          onPress={() => navigation.navigate("Recipes")}
-        >
-          <Text style={styles.textInput}>Our Own's Recipes</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.btn}
-          onPress={() => navigation.navigate("Snack")}
-        >
-          <Text style={styles.textInput}>Snack Facts</Text>
-        </TouchableOpacity>
+        <HStack mb={2}>
+          <Box bg={"white"} borderRadius={30} mr="5">
+            <Image source={Chef} alt={"img"} size="xs" />
+          </Box>
+          <TouchableOpacity onPress={() => navigation.navigate("Recipes")}>
+            <Box>
+              <Text
+                fontSize={20}
+                fontWeight={"bold"}
+                fontFamily={"Graphik-Medium"}
+                color={"white"}
+              >
+                Our Own Recipes
+              </Text>
+            </Box>
+          </TouchableOpacity>
+        </HStack>
+
+        <HStack mb={2}>
+          <Box bg={"white"} borderRadius={30} mr="5">
+            <Image source={FactsImg} alt={"img"} size="xs" />
+          </Box>
+          <TouchableOpacity onPress={() => navigation.navigate("Snack")}>
+            <Box>
+              <Text
+                fontSize={20}
+                fontWeight={"bold"}
+                fontFamily={"Graphik-Medium"}
+                color={"white"}
+              >
+                Snack Facts
+              </Text>
+            </Box>
+          </TouchableOpacity>
+        </HStack>
       </View>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // alignItems: "center",
-    backgroundColor: "black",
-  },
-  imgSection: {
-    flex: 4,
-  },
-  Img: {
-    flex: 2,
-    marginBottom: 10,
-  },
-  textTitle: {
-    alignItems: "center",
-  },
-  textTitle2: {
-    color: "#A8B0B6",
-    fontSize: 40,
-    fontFamily: "Graphik-Medium",
-  },
-  textInput: {
-    fontSize: 20,
-    color: "white",
-    fontWeight: "bold",
-    fontFamily: "Graphik-Medium",
-  },
-  midText: {
-    flex: 1,
-  },
-  textInputHead: {
-    // flex: 1,
-    fontSize: 20,
-    color: "white",
-    fontWeight: "bold",
-    fontFamily: "Graphik-Medium",
-  },
-  btnContainer: {
-    flex: 1.5,
-  },
-  btn: {
-    margin: 5,
-  },
-});
